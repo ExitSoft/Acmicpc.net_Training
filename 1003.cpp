3a4181fe424d849197fffd9d45863ce53168e546
Na Yun Hwan <kbk9288@gmail.com>
2015-04-28 00:56:00 +0900

    Update 1003.cpp
    주석 제거

--- 1003.cpp
+++ 1003.cpp
@@ -1,34 +1,7 @@
 #include <stdio.h>
 
-// int *one = new int[count];
-// int *zero = new int[count];
 int one[100000] = {0};
 int zero[100000] = {0};
-// int fibo_zero(int n, int zero_count){
-// 	if(n == 0){
-// 		zero_count += 1;
-// 		return zero_count;
-// 	}
-// 	else if(n == 1){
-// 		return 0;
-// 	}
-// 	else{
-// 		return fibo_zero(n-1, zero_count) + fibo_zero(n-2, zero_count);
-// 	}
-// }
-
-// int fibo_one(int n, int one_count){
-// 	if(n == 0){
-// 		return 0;
-// 	}
-// 	else if(n == 1){
-// 		one_count += 1;
-// 		return one_count;
-// 	}
-// 	else{
-// 		return fibo_one(n-1, one_count) + fibo_one(n-2, one_count);
-// 	}
-// }
 
 int fibo(int n, int i) {
 	if (n==0) {
