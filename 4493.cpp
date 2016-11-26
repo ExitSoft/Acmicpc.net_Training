4c771549f5af287bafcd1ecdaa71eaa9d3239bd5
Na Yun Hwan <kbk9288@gmail.com>
2016-11-27 01:45:57 +0900

    Add 4493

--- 4493.cpp
+++ 4493.cpp
@@ -1,25 +1,63 @@
 #include <stdio.h>
 
 int main(){
+
 	int cnt;
+	
+
 	scanf("%d", &cnt);
 	while(cnt--){
 		int n;
-		int winA = 0, winB = 0;
 		
+		int winA = 0, winB = 0;	
 
 		scanf("%d", &n);
+
 		for(int i = 0; i < n; i++){
 			char a[2], b[2];
 			scanf("%s %s", a, b);
 
-			printf("%s %s\n", a, b);
+			if(a[0] == b[0]) continue;
+			if(a[0] == 'R'){
+				if(b[0] == 'S'){
+					winA++;
+					continue;
+				}
+				else if(b[0] == 'P'){
+					winB++;
+					continue;
+				}
+			}
+			else if(a[0] == 'S'){
+				if(b[0] == 'R'){
+					winB++;
+					continue;
+				}
+				
+				else if(b[0] == 'P'){
+					winA++;
+					continue;
+				}	
+			}
+			else if(a[0] == 'P'){
+				if(b[0] == 'R'){
+					winA++;
+					continue;
+				}
+				else if(b[0] == 'S'){
+					winB++;
+					continue;	
+				} 
+			}	
 
-			
+
 		}
 		
-
+		if(winA > winB) printf("Player 1\n");
+		else if(winA < winB) printf("Player 2\n");
+		else printf("TIE\n");
 	}
+
 
 	return 0;
 }
