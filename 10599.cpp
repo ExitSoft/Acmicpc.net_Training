bf93c99e005301e21d8557411ecbe70b1d175730
Na Yun Hwan <kbk9288@gmail.com>
2016-11-29 13:32:44 +0900

    Add 10599

---
+++ 10599.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int abs(int n){
+	return (n < 0)? -n : n;
+}
+
+int main(){
+	while(1){
+		int a, b, c, d;
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+		if(a == 0 && b == 0 && c == 0 && d == 0) break;
+		printf("%d %d\n", abs(b-c), abs(a-d));
+
+
+	}
+	return 0;
+}
