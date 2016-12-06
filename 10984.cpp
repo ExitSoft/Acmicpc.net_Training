1b8d1f4aafa1dbc067b07b705a1621473705e9ae
Na Yun Hwan <nayunhwan@Naui-MacBook-Pro.local>
2016-12-06 21:07:43 +0900

    Add 10984

---
+++ 10984.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int cnt;
+	scanf("%d", &cnt);
+	while(cnt--){
+		int n;
+		scanf("%d", &n);
+
+		int divide = 0;
+		float sum = 0;
+		for(int i = 0; i < n; i++){
+			int degree;
+			float score;
+
+			scanf("%d %f", &degree, &score);
+			divide += degree;
+			sum += (degree * score);
+		}
+
+		printf("%d %.1f\n", divide, sum/divide);
+	}
+	return 0;
+}
