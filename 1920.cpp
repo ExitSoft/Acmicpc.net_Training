a6d82f8e1e99dd24c2330d60e5cec3585844cf86
nayunhwan <nayunhwan.dev@gmail.com>
2017-04-02 17:38:53 +0900

    Update 1920

--- 1920.cpp
+++ 1920.cpp
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100001];
+int binSearch(int start, int end, int x) {
+	int mid = (start + end) / 2;
+	if(start > end) return 0;
+	return (arr[mid] == x)? 1 : (x < arr[mid])? binSearch(start, mid-1, x) : binSearch(mid+1, end, x);
+}
 
 int main(void){
+	int n, c;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-	int count;
-	scanf("%d", &count);
-	for(int i = 0; i < count; i++){
-		
-	}
+	sort(arr, arr+n);
 
+	scanf("%d", &c);
+	for(int i = 0; i < c; i++){
+		int input;
+		scanf("%d", &input);
+		printf("%d\n", binSearch(0, n, input));	
+	} 
 	return 0;
 }
