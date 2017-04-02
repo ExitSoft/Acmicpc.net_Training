9005f1c2995b444c1b28cdcfd2dd4761b0425b3c
nayunhwan <nayunhwan.dev@gmail.com>
2017-04-02 17:16:24 +0900

    Update 1874

--- 1874.cpp
+++ 1874.cpp
@@ -1,111 +1,37 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stack>
+#include <queue>
 
-int stack[1000];
-int top = -1;
-
-int pop_stack[1000];
-int top2 = -1;
-
-char char_stack[1000];
-int top3 = -1;
-
-void char_push(char n){
-	char_stack[++top3] = n;
-}
-
-
-void push2(int n){
-	pop_stack[++top2] = n;
-}
-
-int pop2(){
-	return pop_stack[top2--];
-}
-
-bool empty(){
-	return top == -1;
-}
-
-void push(int n){
-	// printf("+\n");
-	char_push('+');
-	stack[++top] = n;
-}
-
-int pop(){
-	if(!empty()){
-		// printf("-\n");
-		char_push('-');
-		push2(stack[top]);
-		return stack[top--];
-	}
-}
-
-
-
+using namespace std;
 
 int main(){
-	int num;
-	int arr[1000];
-	scanf("%d", &num);
+	stack<int> st;
+	queue<char> q;
 
-	for(int i = 0; i < num; i++){
-		scanf("%d", &arr[i]);
-	}
-
-	int count = 1;
-	for(int i = 0 ; i < num; i++){
-
-		while(stack[top] < arr[i] || empty()){
-			push(count++);
-			// printf("count = %d\n", count);
+	int n;
+	scanf("%d", &n);
+	int index = 1;
+	while(n--){
+		int input;
+		scanf("%d", &input);
+		
+		while(st.empty() || st.top() < input){
+			st.push(index++);	
+			q.push('+');
 		}
 
-		while(stack[top] >= arr[i]){
-			// printf("%d\n", pop());
-			// printf("count = %d\n", count);
-			// push2(pop());
-			pop();
+		if(st.top() == input){
+			st.pop();
+			q.push('-');
 		}
-
-
-		// if(count == arr[i]){
-		// 	printf("i = ? %d\n", arr[i]);
-		// 	pop();
-		// 	// break;
-		// }
-
-		// if(stack[top] == arr[i]){
-		// 	printf("%d\n", pop());
-		// }
-		// if(empty()){
-		// 	flag = true;
-		// 	break;
-		// }
-
-	}
-	bool flag = true;
-
-	// if(top2+1 != num){
-	// 	printf("NO\n");
-	// }
-	// printf("\n\n");
-	for(int i = 0; i < num; i++){
-		if(arr[i] != pop_stack[i]){
-			// printf("NO\n");
-			flag = false;
-			break;
-		}
-		// printf("%d\n", pop_stack[i]);
-	}
-
-	if(flag){
-		for(int i = 0; i < top3+1; i++){
-			printf("%c\n", char_stack[i]);
+		else {
+			printf("NO\n");
+			return 0;
 		}
 	}
-	else{
-		printf("NO\n");
+	while(!q.empty()){
+		printf("%c\n", q.front());
+		q.pop();
 	}
 	return 0;
 }
