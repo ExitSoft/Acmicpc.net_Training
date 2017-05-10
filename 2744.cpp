62bf2639ecef8bf050de97f98b57fca15ba0be24
nayunhwan <nayunhwan.dev@gmail.com>
2017-05-10 20:51:36 +0900

    Add 2744

---
+++ 2744.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char str[101];
+	scanf("%s", str);
+	int len = strlen(str);
+	for(int i = 0; i < len; i++){
+		if(str[i] >= 'a' && str[i] <= 'z'){
+			str[i] -= 'a';
+			str[i] += 'A';
+		}
+		else{
+			str[i] -= 'A';
+			str[i] += 'a';
+		}
+	}
+	printf("%s\n", str);
+	return 0;
+}
